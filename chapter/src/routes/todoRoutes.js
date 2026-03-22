74698182549247026a1d75085170c5b76a26fb05
dankomah15 <ankomahdaniel995@gmail.com>
2026-03-22 04:24:12 +0000

    first commit

--- chapter/src/routes/todoRoutes.js
+++ chapter/src/routes/todoRoutes.js
@@ -13,14 +13,39 @@ router.get('/', (req,res) => {
   
 
 //create a new todo
-router.post('/', (req,res) => {})
+router.post('/', (req,res) => {
+     const {task} = req.body
+     const insertTodo = db.prepare(` INSERT INTO todos (user_id, task) VALUES (?, ?)`)
+     const result = insertTodo.run(req.userId, task)
+
+     res.json({ id: result.lastInsertRowid, task, completed: 0 })
+})
+
+
 
 //update a todo
 
-router.put('/:id',(req, res) => {})
+router.put('/:id',(req, res) => {
+     const {completed} = req.body
+     const { id } = req.params
+     const { page } = req.query
+
+     const updatedTodo = db.prepare('UPDATE todos SET completed = ? WHERE id = ?')
+
+     updatedTodo.run(completed, id)
+
+     res.json({messege: 'Todo completed'})
+})
 
 //delete a todo
 
-router.delete('/:id',(req, res) => {})
+router.delete('/:id',(req, res) => {
+     const {id} = req.params
+     const userId = req.userId
+     const deleteTodo = db.prepare('DELETE FROM todos WHERE id = ? AND user_id = ?')
+
+     deleteTodo.run(id, userId)
+
+})
 
 export default router
